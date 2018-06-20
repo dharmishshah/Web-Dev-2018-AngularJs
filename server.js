2211ffe53c37ea36adeb9efde738d522fbd745b0
dharmish <shah.dharm@husky.neu.edu>
2018-06-19 23:43:04 -0400

    remote node server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -8,7 +9,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/src/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
